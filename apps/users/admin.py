4a47c928ca03b0621dc7e14038e90d39c2a26f2c
Tasin <tasintsn@gmail.com>
2025-04-16 01:55:17 +0600

    Add User model and admin configuration with initial migration

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the User model.
+    """
+    list_display = ('username', 'email', 'organization', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active')
+    ordering = ('username',)
+
+admin.site.register(User, UserAdmin)
