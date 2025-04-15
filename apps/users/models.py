5b431c69595620d177b515ef4960ce506f3d16a0
Tasin <tasintsn@gmail.com>
2025-04-16 01:45:33 +0600

    Refactor user app configuration and implement custom user model with organization, groups, and permissions

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group, Permission
+from apps.organizations.models import Organization
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom user model that extends the default Django user model.
+    """
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name='users',
+        null=True,
+        blank=True
+    )
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",  # <-- avoid reverse accessor clash
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions_set",  # <-- avoid reverse accessor clash
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
+
+    def __str__(self):
+        return self.username
