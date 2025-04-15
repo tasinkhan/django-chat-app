ea0db6ceb8fa715f99c54d50a5565bd038668e18
Tasin <tasintsn@gmail.com>
2025-04-16 02:02:41 +0600

    Update UserAdmin to include 'role' in list_display

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 from .models import User
 
+
 class UserAdmin(admin.ModelAdmin):
     """
     Admin interface for the User model.
     """
-    list_display = ('username', 'email', 'organization', 'is_staff', 'is_active')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    list_filter = ('is_staff', 'is_active')
-    ordering = ('username',)
+
+    list_display = (
+        "username",
+        "email",
+        "organization",
+        "is_staff",
+        "is_active",
+        "role",
+    )
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_filter = ("is_staff", "is_active")
+    ordering = ("username",)
+
 
 admin.site.register(User, UserAdmin)
