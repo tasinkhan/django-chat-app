9dd840f8e513364bf0227bb45001e00fa1a70a08
Tasin <tasintsn@gmail.com>
2025-04-17 02:35:11 +0600

    Fix db_for_read method in MasterSlaveRouter to return 'replica' and refine allow_relation logic for better database handling

--- apps/core/db_router.py
+++ apps/core/db_router.py
@@ -1,12 +1,16 @@
 class MasterSlaveRouter:
     def db_for_read(self, model, **hints):
-        return "default"
+        return "replica"
 
     def db_for_write(self, model, **hints):
         return "default"
 
     def allow_relation(self, obj1, obj2, **hints):
-        return True
+        # Allow any relation if both models are in the same DB
+        db_list = ('default', 'replica')
+        if obj1._state.db in db_list and obj2._state.db in db_list:
+            return True
+        return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         return db == "default"
